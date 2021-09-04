8702e21ed70cbaa0e9b743cd73473956c8da5e14
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-04 20:54:35 +0100

    ✨ cart item remove function dispatched

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+
+import * as cartActions from '../../redux/actions/cart.action';
 
 const CartScreen = props => {
    const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -18,8 +20,11 @@ const CartScreen = props => {
             sum: state.cart.items[key].sum
          });
       }
-      return transformedCartItems;
+      return transformedCartItems.sort((a, b) =>
+         a.productId > b.productId ? 1 : -1
+      );
    });
+   const dispatch = useDispatch();
 
 
    return (
@@ -43,7 +48,9 @@ const CartScreen = props => {
                   quantity={itemData.item.quantity}
                   title={itemData.item.productTitle}
                   amount={itemData.item.sum}
-                  onRemove={() => { }}
+                  onRemove={() => {
+                     dispatch(cartActions.removeFromCart(itemData.item.productId));
+                  }}
                />
             )}
          />
