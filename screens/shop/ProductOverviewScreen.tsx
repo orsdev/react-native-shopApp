de3ca72d1467689d6d793843ba27aa15b43cdd1d
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-04 18:05:02 +0100

    💄 products card styled

--- screens/shop/ProductOverviewScreen.tsx
+++ screens/shop/ProductOverviewScreen.tsx
@@ -6,24 +6,50 @@ import { RootState } from '../../redux/rootReducer';
 
 import ProductItem from '../../components/shop/ProductItem';
 
-const ProductOverviewScreen = () => {
-   const products = useSelector((state: RootState) => state.products.availableProducts);
+type ProductType = {
+   imageUrl: string;
+   title: string;
+   price: number;
+};
 
-   const renderProducts = ({ item }: {
-      item: {
-         title?: string;
-      }
-   }) => (
-      <Text>{item.title}</Text>
-   )
+const ProductOverviewScreen = ({ navigation }: {
+   navigation: any
+}) => {
+   const products: ProductType | object[] = useSelector((state: RootState) => state.products.availableProducts);
+
+   if (products.length === 0) {
+      return (<View style={{
+         flex: 1,
+         alignItems: 'center',
+         justifyContent: 'center'
+      }}>
+         <Text style={{
+            fontSize: 20
+         }}>Product not available</Text>
+      </View>)
+   }
 
    return (
       <View>
          <FlatList
             data={products}
-            renderItem={renderProducts}
+            renderItem={(itemData: { item: any }) => {
+               return (
+                  <ProductItem
+                     image={itemData.item.imageUrl}
+                     title={itemData.item.title}
+                     price={itemData.item.price}
+                     onViewDetail={() => {
+                        navigation.navigate('ProductDetail', {
+                           productId: itemData.item.id,
+                           productTitle: itemData.item.title
+                        })
+                     }}
+                     onAddToCart={() => { }}
+                  />
+               )
+            }}
          />
-         <ProductItem />
       </View>
    )
 }
