4a285d0a4db5c76d975b617e769e3479e7cdcde4
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-04 18:03:57 +0100

    ✨ go to product detail page when card is clicked

--- components/shop/ProductItem.tsx
+++ components/shop/ProductItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Button, NativeSyntheticEvent, NativeTouchEvent } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, NativeSyntheticEvent, NativeTouchEvent, Pressable } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
@@ -11,27 +11,29 @@ const ProductItem = (props: {
    onAddToCart: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void
 }) => {
    return (
-      <View style={styles.product}>
-         <View style={styles.imageContainer}>
-            <Image style={styles.image} source={{ uri: props.image }} />
+      <Pressable onPress={props.onViewDetail}>
+         <View style={styles.product}>
+            <View style={styles.imageContainer}>
+               <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+               <Text style={styles.title}>{props.title}</Text>
+               <Text style={styles.price}>${props.price ? props.price.toFixed(2) : ''}</Text>
+            </View>
+            <View style={styles.actions}>
+               <Button
+                  color={Colors.primary}
+                  title="View Details"
+                  onPress={props.onViewDetail}
+               />
+               <Button
+                  color={Colors.primary}
+                  title="To Cart"
+                  onPress={props.onAddToCart}
+               />
+            </View>
          </View>
-         <View style={styles.details}>
-            <Text style={styles.title}>{props.title}</Text>
-            <Text style={styles.price}>${props.price ? props.price.toFixed(2) : ''}</Text>
-         </View>
-         <View style={styles.actions}>
-            <Button
-               color={Colors.primary}
-               title="View Details"
-               onPress={props.onViewDetail}
-            />
-            <Button
-               color={Colors.primary}
-               title="To Cart"
-               onPress={props.onAddToCart}
-            />
-         </View>
-      </View>
+      </Pressable>
    )
 }
 
