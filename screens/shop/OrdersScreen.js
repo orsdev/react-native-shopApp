fa85f14f7e1196d7c3c7a9ee99088cf52669cb7b
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 09:16:34 +0100

    ✨ ordersscreen added

---
+++ screens/shop/OrdersScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FlatList, Text, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { DrawerActions } from '@react-navigation/native';
+
+import { CustomHeader } from '../../components/ui/HeaderButton';
+import { Item } from 'react-navigation-header-buttons';
+
+const OrdersScreen = props => {
+   const orders = useSelector(state => state.orders.orders);
+
+   React.useLayoutEffect(() => {
+      props.navigation.setOptions({
+         headerLeft: () => (
+            <CustomHeader>
+               <Item title="Menu" iconName="menu" onPress={() => {
+                  props.navigation.dispatch(DrawerActions.openDrawer());
+               }} />
+            </CustomHeader>
+         ),
+      });
+   }, [props.navigation])
+
+   return (
+      <FlatList
+         data={orders}
+         keyExtractor={item => item.id}
+         renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+      />
+      // <Text style={{
+      //    fontSize: 30,
+      //    textAlign: 'center',
+      //    padding: 20
+      // }}>Orders</Text>
+   );
+};
+
+export default OrdersScreen;
