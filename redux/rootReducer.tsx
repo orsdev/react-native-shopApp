71b9b1c16d8a6bebdbcccf1906998715c27c6b95
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-05 10:35:37 +0100

    ✨ orders added to rootReducer

--- redux/rootReducer.tsx
+++ redux/rootReducer.tsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import productReducer from './reducers/product.reducer';
 import cartReducer from './reducers/cart.reducer';
+import ordersReducer from './reducers/orders';
 
 export const rootReducer = combineReducers({
    cart: cartReducer,
-   products: productReducer
+   products: productReducer,
+   orders: ordersReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
